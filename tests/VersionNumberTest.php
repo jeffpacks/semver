071450e7f6f79669c8796e1be4bcad5cfc857ec9
Johan Fredrik Varen <johan@muuh.no>
2022-03-03 13:42:03 +0100

    Requiring PHPUnit 9. Added unit tests

---
+++ tests/VersionNumberTest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace jeffpacks\semver\tests;
+
+use jeffpacks\semver\VersionNumber;
+use PHPUnit\Framework\TestCase;
+
+class VersionNumberTest extends TestCase {
+
+	public function testIsStable() {
+
+		$versionNumber = new VersionNumber('0');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('0.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('0.1.0');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('0.1.0.0');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('0-alpha');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('0-alpha.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('0.0-alpha');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('0.0-alpha.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('0.0.0-alpha');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('0.0.0-alpha.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1-alpha');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1-alpha.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0-alpha');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0-alpha.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0.0-alpha');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0.0-alpha.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1-beta.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0-beta');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0-beta.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0.0-beta');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0.0-beta.1');
+		$this->assertFalse($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1');
+		$this->assertTrue($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0');
+		$this->assertTrue($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0.0');
+		$this->assertTrue($versionNumber->isStable());
+
+		$versionNumber = new VersionNumber('1.0.0.0');
+		$this->assertTrue($versionNumber->isStable());
+
+	}
+
+	/**
+	 * @throws \Exception
+	 */
+	public function testIncrement() {
+
+		$versionNumber = new VersionNumber('1');
+		$versionNumber->increment();
+		$this->assertEquals('2', (string) $versionNumber);
+		$versionNumber->increment(VersionNumber::MAJOR);
+		$this->assertEquals('3', (string) $versionNumber);
+		$versionNumber->increment(VersionNumber::MINOR);
+		$this->assertEquals('3', (string) $versionNumber);
+		$versionNumber->increment(VersionNumber::PATCH);
+		$this->assertEquals('3', (string) $versionNumber);
+		$versionNumber->increment(VersionNumber::AUX);
+		$this->assertEquals('3', (string) $versionNumber);
+
+		$versionNumber = new VersionNumber('0.0');
+		$versionNumber->increment();
+		$this->assertEquals('0.1', (string) $versionNumber);
+
+		$versionNumber = new VersionNumber('1.0');
+		$versionNumber->increment();
+		$this->assertEquals('1.1', (string) $versionNumber);
+
+		$versionNumber = new VersionNumber('1.0.0');
+		$versionNumber->increment();
+		$this->assertEquals('1.0.1', (string) $versionNumber);
+
+		$versionNumber = new VersionNumber('1.0.0.0');
+		$versionNumber->increment();
+		$this->assertEquals('1.0.0.1', (string) $versionNumber);
+
+	}
+}
