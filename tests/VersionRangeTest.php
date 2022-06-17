a31c5648a47145491a78fe9897648a13780450df
Johan Fredrik Varen <johan.fredrik@keystone.no>
2022-06-18 01:19:30 +0200

    Mixed the order of the version number set in VersionRangeTest::testGetHightestMatch() to ensure order does not matter

--- tests/VersionRangeTest.php
+++ tests/VersionRangeTest.php
@@ -13,13 +13,13 @@ class VersionRangeTest extends TestCase {
 		$range = new VersionRange('^2.1.2');
 
 		$versionNumbers = [
+			'3.0.0',
+			'2.2.0',
 			'1.0.0',
-			'1.2.0',
 			'2.0.0',
+			'1.2.0',
 			'2.1.0',
 			'2.1.1',
-			'2.2.0',
-			'3.0.0'
 		];
 
 		$this->assertEquals('2.2.0', $range->getHighestMatch($versionNumbers));
